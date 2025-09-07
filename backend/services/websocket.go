f3b319541484d87a8e7b7b46e83847203b650c9d
Tomas Alvarez <tomasignacioalv@hotmail.com>
2025-09-07 19:14:46 -0300

    add WebSocketConnection struct with mutex for thread-safe operations

--- backend/services/websocket.go
+++ backend/services/websocket.go
@@ -1,7 +1,18 @@
 package services
 
+import (
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
 type IWebsocketConnection interface {
 	SendMessage(message []byte) error
 	ReceiveMessage() ([]byte, error)
 	Close() error
+}
+
+type WebSocketConnection struct {
+	conn  *websocket.Conn
+	mutex sync.Mutex
 }
